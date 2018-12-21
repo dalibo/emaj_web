4a1380a2ef374f5ab68c35a0df86b8a8f8818ffe
Philippe Beaudoin <phb.emaj@free.fr>
2018-12-21 19:34:09 +0100

    Plugin deconstruction step 6: move the multiactionform.js file from plugins/Emaj/js toward ./js. Also remove the absolete ac_insert_row.js component.

--- classes/Misc.php
+++ classes/Misc.php
@@ -475,7 +475,7 @@
 				echo "<link rel=\"shortcut icon\" href=\"images/themes/{$conf['theme']}/Favicon.ico\" type=\"image/vnd.microsoft.icon\" />\n";
 				echo "<link rel=\"icon\" type=\"image/png\" href=\"images/themes/{$conf['theme']}/EmajwebIcon.png\" />\n";
 				// Javascript
-				echo "<script type=\"text/javascript\" src=\"plugins/Emaj/js/multiactionform.js\"></script>\n";
+				echo "<script type=\"text/javascript\" src=\"js/multiactionform.js\"></script>\n";
 				echo "<script type=\"text/javascript\" src=\"libraries/js/jquery-3.3.1.min.js\"></script>\n";
 				echo "<script type=\"text/javascript\" src=\"libraries/js/jquery.tablesorter.min.js\"></script>\n";
 				echo "<script type=\"text/javascript\" src=\"libraries/js/jquery.tablesorter.widgets.min.js\"></script>\n";
@@ -2172,108 +2172,6 @@
 			}
 			
 			echo "</td></tr></table>\n";
-		}
-
-		/**
-		 * returns an array representing FKs definition for a table, sorted by fields
-		 * or by constraint.
-		 * @param $table The table to retrieve FK contraints from
-		 * @returns the array of FK definition:
-		 *   array(
-		 *     'byconstr' => array(
-		 *       constrain id => array(
-		 *         confrelid => foreign relation oid
-		 *         f_schema => foreign schema name
-		 *         f_table => foreign table name
-		 *         pattnums => array of parent's fields nums
-		 *         pattnames => array of parent's fields names
-		 *         fattnames => array of foreign attributes names
-		 *       )
-		 *     ),
-		 *     'byfield' => array(
-		 *       attribute num => array (constraint id, ...)
-		 *     ),
-		 *     'code' => HTML/js code to include in the page for auto-completion
-		 *   )
-		 **/
-		function getAutocompleteFKProperties($table) {
-			global $data;
-
-			$fksprops = array(
-				'byconstr' => array(),
-				'byfield' => array(),
-				'code' => ''
-			);
-
-			$constrs = $data->getConstraintsWithFields($table);
-
-			if (!$constrs->EOF) {
-				$conrelid = $constrs->fields['conrelid'];
-				while(!$constrs->EOF) {
-					if ($constrs->fields['contype'] == 'f') {
-						if (!isset($fksprops['byconstr'][$constrs->fields['conid']])) {
-							$fksprops['byconstr'][$constrs->fields['conid']] = array (
-								'confrelid' => $constrs->fields['confrelid'],
-								'f_table' => $constrs->fields['f_table'],
-								'f_schema' => $constrs->fields['f_schema'],
-								'pattnums' => array(),
-								'pattnames' => array(),
-								'fattnames' => array()
-							);
-						}
-
-						$fksprops['byconstr'][$constrs->fields['conid']]['pattnums'][] = $constrs->fields['p_attnum'];
-						$fksprops['byconstr'][$constrs->fields['conid']]['pattnames'][] = $constrs->fields['p_field'];
-						$fksprops['byconstr'][$constrs->fields['conid']]['fattnames'][] = $constrs->fields['f_field'];
-
-						if (!isset($fksprops['byfield'][$constrs->fields['p_attnum']]))
-							$fksprops['byfield'][$constrs->fields['p_attnum']] = array();
-						$fksprops['byfield'][$constrs->fields['p_attnum']][] = $constrs->fields['conid'];
-					}
-					$constrs->moveNext();
-				}
-
-				$fksprops['code'] = "<script type=\"text/javascript\">\n";
-				$fksprops['code'] .= "var constrs = {};\n";
-				foreach ($fksprops['byconstr'] as $conid => $props) {
-					$fksprops['code'] .= "constrs.constr_{$conid} = {\n";
-					$fksprops['code'] .= 'pattnums: ['. implode(',',$props['pattnums']) ."],\n";
-					$fksprops['code'] .= "f_table:'". addslashes(htmlentities($props['f_table'], ENT_QUOTES, 'UTF-8')) ."',\n";
-					$fksprops['code'] .= "f_schema:'". addslashes(htmlentities($props['f_schema'], ENT_QUOTES, 'UTF-8')) ."',\n";
-					$_ = '';
-					foreach ($props['pattnames'] as $n) {
-						$_ .= ",'". htmlentities($n, ENT_QUOTES, 'UTF-8') ."'";
-					}
-					$fksprops['code'] .= 'pattnames: ['. substr($_, 1) ."],\n";
-
-					$_ = '';
-					foreach ($props['fattnames'] as $n) {
-						$_ .= ",'". htmlentities($n, ENT_QUOTES, 'UTF-8') ."'";
-					}
-
-					$fksprops['code'] .= 'fattnames: ['. substr($_, 1) ."]\n";
-					$fksprops['code'] .= "};\n";
-				}
-
-				$fksprops['code'] .= "var attrs = {};\n";
-				foreach ($fksprops['byfield'] as $attnum => $cstrs ) {
-					$fksprops['code'] .= "attrs.attr_{$attnum} = [". implode(',', $fksprops['byfield'][$attnum]) ."];\n";
-				}
-
-				$fksprops['code'] .= "var table='". addslashes(htmlentities($table, ENT_QUOTES, 'UTF-8')) ."';";
-				$fksprops['code'] .= "var server='". htmlentities($_REQUEST['server'], ENT_QUOTES, 'UTF-8') ."';";
-				$fksprops['code'] .= "var database='". addslashes(htmlentities($_REQUEST['database'], ENT_QUOTES, 'UTF-8')) ."';";
-				$fksprops['code'] .= "</script>\n";
-
-				$fksprops['code'] .= '<div id="fkbg"></div>';
-				$fksprops['code'] .= '<div id="fklist"></div>';
-				$fksprops['code'] .= '<script src="js/ac_insert_row.js" type="text/javascript"></script>';
-			}
-
-			else /* we have no foreign keys on this table */
-				return false;
-
-			return $fksprops;
 		}
 	}
 ?>

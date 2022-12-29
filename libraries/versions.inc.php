987c0231c6bd3568ee2dfdaeca01369dea248720
Philippe Beaudoin <phb.emaj@free.fr>
2022-12-29 16:03:45 +0100

    Remove the support of PHP5.

--- libraries/versions.inc.php
+++ libraries/versions.inc.php
@@ -10,7 +10,7 @@
 
 	// PostgreSQL and PHP minimum version
 	$postgresqlMinVer = '9.1';
-	$phpMinVer = '5.0';
+	$phpMinVer = '7.0';
 
 	// E-Maj versions
 	$oldest_supported_emaj_version = '1.3.0';			// Oldest supported emaj version

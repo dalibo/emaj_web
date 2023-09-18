1c164662658ead6f0a115124ce80490521bbaad5
Philippe Beaudoin <phb.emaj@free.fr>
2023-09-18 10:45:33 +0200

    Setup the new 4.3.0 version

--- libraries/versions.inc.php
+++ libraries/versions.inc.php
@@ -6,7 +6,7 @@
 	 */
 
 	// Emaj_web current version
-	$appVersion = '4.2';
+	$appVersion = '4.3';
 
 	// PostgreSQL and PHP minimum version
 	$postgresqlMinVer = '9.1';
@@ -15,8 +15,8 @@
 	// E-Maj versions
 	$oldest_supported_emaj_version = '2.1.0';			// Oldest supported emaj version
 	$oldest_supported_emaj_version_num = 20100;
-	$last_known_emaj_version = '4.2.0';					// Most recent known emaj version
-	$last_known_emaj_version_num = 40200;
+	$last_known_emaj_version = '4.3.0';					// Most recent known emaj version
+	$last_known_emaj_version_num = 40300;
 
 	// Cross references between the emaj extensions (as string) and the Postgres major versions (as string)
 	$xrefEmajPg['2.0.0']['minPostgresVersion'] = '9.1'; $xrefEmajPg['2.0.0']['maxPostgresVersion'] = '9.6';
@@ -37,5 +37,6 @@
 	$xrefEmajPg['4.0.1']['minPostgresVersion'] = '9.5'; $xrefEmajPg['4.0.1']['maxPostgresVersion'] = '14';
 	$xrefEmajPg['4.1.0']['minPostgresVersion'] = '9.5'; $xrefEmajPg['4.1.0']['maxPostgresVersion'] = '15';
 	$xrefEmajPg['4.2.0']['minPostgresVersion'] = '11'; $xrefEmajPg['4.2.0']['maxPostgresVersion'] = '16';
+	$xrefEmajPg['4.3.0']['minPostgresVersion'] = '11'; $xrefEmajPg['4.3.0']['maxPostgresVersion'] = '16';
 
 ?>
